03faaac80109ae38d968e54e66ccbe6053e9e507
natigh <ntgrc1113@gmail.com>
2025-05-03 12:31:01 -0500

    base

---
+++ LandingPage/config.php
@@ -0,0 +1,10 @@
+<?php
+define('URL_PUBLIC_FOLDER', 'public');
+define('URL_PROTOCOL', 'http://');
+define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
+define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME'])));
+define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
+
+define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('APP', ROOT . 'application' . DIRECTORY_SEPARATOR);
+
---
+++ LandingPage/index.php
@@ -0,0 +1,79 @@
+<?php
+    require_once("config.php");
+    //die ("url".URL);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Milihuellas</title>
+    <link rel="icon" href="<?php echo URL;?>/img/Logo.png"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style/index.css">
+        
+        <meta name="author" content="Videojuegos & Desarrollo">
+        <meta name="description" content="Ejemplo de formulario de acceso basado en HTML5 y CSS">
+        <meta name="keywords" content="login,formulariode acceso html">
+
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Montez&display=swap');
+        </style>
+</head>
+<body>
+    <header>
+        <div class="box">
+            <img src="img/kicks.gif" alt="Logo" class="picture">
+        </div>
+        <div class="title">
+            <h1>Milihuellas</h1>
+            <p>Inspira en cada paso que das</p>
+        </div>
+        <div class="buttons">
+            <button><a href="AboutUs.php">Conócenos</a></button>
+            <button><a href="https://web.whatsapp.com/" target="_blank"><img src="img/redes/icons8-whatsapp.gif" alt="whatsapp"/></a></button>
+            <button><a href="ListaCompras.php"><img src="img/redes/icons8-agregar-a-carrito-de-compras.gif" alt="listaCompra"></a></button>
+        </div>
+    </header>
+    <section class="marcas">    
+        <h3>Nuestras marcas</h3>
+        <ul>
+            <a href="marcas/Adidas.php">Adidas</a><br>
+            <a href="marcas/Nike.php">Nike</a><br>
+            <a href="marcas/Puma.php">Puma</a><br>
+            <a href="marcas/Skechers.php">Skechers</a><br>
+            <a href="marcas/Reebok.php">Reebok</a><br>
+            <a href="marcas/UnderArmour.php">Under Armour</a><br>
+            <a href="marcas/Fila.php">Fila</a><br>
+            <a href="marcas/Personalizados.php">Personalizados</a>
+        </ul>
+    </section>
+    <div class="redes">
+        <a href="https://instagram.com" target="_blank"><img src="img/redes/icons8-instagram.gif" alt="instagram"/></a><br>
+        <a href="https://facebook.com" target="_blank"><img src="img/redes/icons8-facebook.gif" alt="facebook"/></a><br>
+        <a href="https://tiktok.com" target="_blank"><img src="img/redes/icons8-tik-tok.gif" alt="Tiktok"/></a><br>   
+    </div>
+
+    <footer>
+        <div>
+            <img src="img/redes/icons8-reloj.gif" alt="Horario"/><br>
+            <p>
+             Lunes a Sabado 10:00 a.m. a 8:00 p.m. <br>
+        </div>
+        <div>
+            <a href="email.php"><img src="img/redes/icons8-mensaje.gif" alt="e-mail"/></a><br>
+            <p>
+                Contacto: Milihuellas@gmail.com
+            </p>
+        </div>
+        <div>
+            <a href="preguntas.php"><img src="img/redes/icons8-pregunta.gif" alt="preguntas frecuentes"/></a><br>
+            <p>
+                Preguntas Frecuentes
+            </p>
+        </div>
+        
+    </footer>
+ 
+</body>
+</html>
